cc2048b334705068e38bcc33290c399832736c85
Carson Halstead <carsonhalstead@gmail.com>
2014-03-24 17:40:01 -0400

    Raw New England
    fix mission statement float
    
    .js added product library

---
+++ onePageShop/scripts/products.js
@@ -0,0 +1,45 @@
+var product = {
+	
+	tee-1: {
+		title: "Fishscale HFA Baseball Tee",
+		price: 25.00,
+		image: "images/fishscale-hfa-tee.jpg",
+		description: "For years, bass fishing has been one of our staple ways of making the best out of the rainy days not spent on a skateboard, so it is only fitting that we release a project within skateboarding as a show of thanks to the freshwater beasts that kept us having fun."
+	},
+
+	tee-2: {
+		title: "Fishscale Box Logo Tee",
+		price: 25.00,
+		image: "images/fishscale-box-logo-tee.jpg",
+		description: "For years, bass fishing has been one of our staple ways of making the best out of the rainy days not spent on a skateboard, so it is only fitting that we release a project within skateboarding as a show of thanks to the freshwater beasts that kept us having fun."
+	},
+
+	tee-3: {
+		title: "Loyalty Over Royalty Longsleeve",
+		price: 28.00,
+		image: "images/lor-tee.jpg",
+		description: "There is a reason why these words are present in our offerings every single season. We stand for things that run far deeper than the surface of skateboarding. A motto is not a motto unless you can live by it on a daily basis."
+	},
+
+	deck-1: {
+		title: "Loyalty Over Royalty Gold Edition",
+		price: 50.00,
+		image: "images/lor-gold.jpg",
+		description: "Seven ply maple construction. Ebony stained top veneer. Manufactured in the United States. Jessup griptape included."
+	},
+
+	deck-2: {
+		title: "Jonah Miller Tigerstripe Camo",
+		price: 50.00,
+		image: "images/tiger-camo.jpg",
+		description: "Seven ply maple construction. Ebony stained top veneer. Manufactured in the United States. Jessup griptape included."
+	},
+
+	deck-3: {
+		title: "This is My Skateboard Gold Edition",
+		price: 50.00,
+		image: "images/tims-gold.jpg",
+		description: "Seven ply maple construction. Ebony stained top veneer. Manufactured in the United States. Jessup griptape included."
+	},
+
+}
