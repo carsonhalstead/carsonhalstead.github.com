99a771d9a680b5f6f7d5ac874bd292328697b3e4
Carson Halstead <carsonhalstead@gmail.com>
2014-05-05 18:37:06 -0400

    Bar Chart
    Beginning the Vis

--- dataVisualizaton/scripts/datavis.js
+++ dataVisualizaton/scripts/datavis.js
@@ -1,3 +1,11 @@
-function () {
-	$( 'body' ).html( '<p>Hello</p>' );
-};
+$(function () {
+	$( 'body' ).append( '<p>This right here</p>' );
+
+	for(var i = 0; i < data.length; i++ ) {
+
+		var columns	=	data[i];
+		var wrapper	=	$( '<div>' );
+		var name	=	$( '<div>' ).text
+	}
+
+})
